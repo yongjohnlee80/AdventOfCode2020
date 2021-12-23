d9d1493f071cfa04a558d1e1f200a09a59156a45
yongjohnlee80 <yongjohnlee80@outlook.com>
2021-12-23 09:14:50 -0800

    Day 15 Easily Defeated.

---
+++ AdventOfCode2020/Day15/AdventDay15Solution.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day15
+{
+    internal class AdventDay15Solution
+    {
+    }
+    /// <summary>
+    /// Type: MemoryGame
+    /// Advent of Code Day 15
+    /// Memory Game with Elves
+    /// In this game, the players take turns saying numbers. They begin by taking turns reading from a list of starting numbers (your puzzle input). 
+    /// Then, each turn consists of considering the most recently spoken number:
+    /// If that was the first time the number has been spoken, the current player says 0.
+    /// Otherwise, the number had been spoken before; the current player announces how many turns apart the number is from when it was previously spoken.
+    /// </summary>
+    internal class MemoryGame
+    {
+        /// <summary>
+        /// Memory Table
+        /// Key: Number Spoken
+        /// Value: Turn
+        /// </summary>
+        protected Dictionary<int, int> numbers = new Dictionary<int,int>();
+        protected int nextNumber; // The Next number that will be spoken by the opponent or player.
+        public int Turn { get; set; } // Turn
+
+        /// <summary>
+        ///  Method: RegisterNum
+        ///  This method automatically increments the Turn variable.
+        ///  Register a spoken number with the memory table.
+        ///  returns the next number that will be spoken.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        protected int RegisterNum(int number)
+        {
+            if(numbers.ContainsKey(number)) // if the number has been spoken before
+            {
+                int temp = numbers[number];
+                numbers[number] = Turn;
+                return Turn++ - temp;
+            }
+            else // if the number is first time spoken
+            {
+                numbers.Add(number, Turn++);
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// Register the starting numbers with the memory table.
+        /// </summary>
+        /// <param name="startingNum"></param>
+        public MemoryGame(int[] startingNum)
+        {
+            Turn = 1;
+            foreach(var i in startingNum)
+            {
+                nextNumber = RegisterNum(i);
+            }
+        }
+
+        /// <summary>
+        /// Method: GetNthNumberSpoken
+        /// Find the number spoken at a specific turn.
+        /// </summary>
+        /// <param name="nthTurn"></param>
+        /// <returns></returns>
+        public int GetNthNumberSpoken(int nthTurn)
+        {
+            while(Turn < nthTurn)
+            {
+                nextNumber = RegisterNum(nextNumber);
+            }
+            return nextNumber;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day15/AdventDay15Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AdventOfCode2020.Day15
+{
+    internal class AdventDay15Test
+    {
+        [Test]
+        public void TestSample()
+        {
+            int[] data = { 0, 3, 6 };
+            MemoryGame test = new MemoryGame(data);
+
+            Assert.That(test.GetNthNumberSpoken(2020), Is.EqualTo(436));
+        }
+
+        [Test]
+        public void TestSamples()
+        {
+            MemoryGame test1 = new MemoryGame(new[] { 1, 3, 2 });
+            Assert.That(test1.GetNthNumberSpoken(2020), Is.EqualTo(1));
+
+            MemoryGame test2 = new MemoryGame(new[] { 2, 1, 3 });
+            Assert.That(test2.GetNthNumberSpoken(2020), Is.EqualTo(10));
+
+            MemoryGame test3 = new MemoryGame(new[] { 1, 2, 3 });
+            Assert.That(test3.GetNthNumberSpoken(2020), Is.EqualTo(27));
+
+            MemoryGame test4 = new MemoryGame(new[] { 2, 3, 1 });
+            Assert.That(test4.GetNthNumberSpoken(2020), Is.EqualTo(78));
+
+            MemoryGame test5 = new MemoryGame(new[] { 3, 2, 1 });
+            Assert.That(test5.GetNthNumberSpoken(2020), Is.EqualTo(438));
+
+            MemoryGame test6 = new MemoryGame(new[] { 3, 1, 2 });
+            Assert.That(test6.GetNthNumberSpoken(2020), Is.EqualTo(1836));
+        }
+        
+        [Test]
+        public void TestPart1Data()
+        {
+            MemoryGame game = new MemoryGame(new[] { 0, 5, 4, 1, 10, 14, 7 });
+
+            Console.WriteLine(game.GetNthNumberSpoken(2020));
+        }
+
+        [Test]
+        public void TestSamplePart2()
+        {
+            MemoryGame test1 = new MemoryGame(new[] { 0, 3, 6 });
+            Assert.That(test1.GetNthNumberSpoken(30_000_000), Is.EqualTo(175594));
+        }
+
+        [Test]
+        public void TestPart2Data()
+        {
+            MemoryGame game2 = new MemoryGame(new[] { 0, 5, 4, 1, 10, 14, 7 });
+
+            Console.WriteLine(game2.GetNthNumberSpoken(30_000_000));
+        }
+    }
+}
