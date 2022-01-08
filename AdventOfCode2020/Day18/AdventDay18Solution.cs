6c57b782575ab16893149ab14416a62b35015950
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-01-07 22:32:32 -0800

    Day 18 Complete!

--- AdventOfCode2020/Day18/AdventDay18Solution.cs
+++ AdventOfCode2020/Day18/AdventDay18Solution.cs
@@ -8,119 +8,301 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2020.Day18
 {
-    internal interface EComponent {}
+    /// <summary>
+    /// Interface: EComponent
+    /// Expression Component
+    /// All Component part must implmemnt Evalute() method that returns Number type
+    /// </summary>
+    internal interface EComponent
+    {
+        public Number Evaluate();
+    }
 
+    /// <summary>
+    /// Type: Number
+    /// Int64 wrapper.
+    /// Change this to Double wrapper for real number computations.
+    /// </summary>
     internal class Number : EComponent
     {
-        public Number(string value)
+        public Number(string value = "0")
         {
             Value = Convert.ToInt64(value);
         }
         public long Value { get; set; }
-    }
-
-    internal class Braket : EComponent
-    {
-        public bool Open { get; set; }
-
-        public Braket(bool isOpen = true)
-        {
-            Open = isOpen;
-        }
-    }
-
-    internal abstract class Operator : EComponent
-    {
-        public abstract long Compute(Number left, Number right);
-    }
-
-    internal class Add : Operator
-    {
-        public override long Compute(Number left, Number right)
-        {
-            return left.Value + right.Value;
-        }
-    }
-
-    internal class Multiply : Operator
-    {
-        public override long Compute(Number left, Number right)
-        {
-            return left.Value * right.Value;
-        }
-    }
-
-    internal class Expression
-    {
-        Queue<EComponent> exprQueue = new Queue<EComponent> ();
-
-        private readonly static string ExpressionRegex = @"([*+/\-)(])|([0-9.]+)";
-
-        protected EComponent Convert(string partOfExpression)
-        {
-            switch (partOfExpression)
-            {
-                case "+": return new Add();
-                case "*": return new Multiply();
-                case "(": return new Braket(isOpen: true);
-                case ")": return new Braket(isOpen: false);
-                default: return new Number(partOfExpression);
-            }
-        }
-
-
-        public void Parse(string expression)
-        {
-            exprQueue.Clear();
-
-            foreach (var temp in Regex.Matches(expression, ExpressionRegex))
-            {
-                exprQueue.Enqueue(Convert(temp.ToString()));
-            }
-        }
 
         public Number Evaluate()
         {
-            var item = exprQueue.Dequeue();
-            Number left;
-            if (item.GetType() == typeof(Braket))
-            {
-                left = Evaluate();
-            }
-            else
-            {
-                left = (Number)item;
-
-            }
-            while (exprQueue.Count > 0)
-            {
-                item = exprQueue.Dequeue();
-                if (item.GetType() == typeof(Braket)) return left;
-                Operator op = (Operator)item;
-                
-                item = exprQueue.Dequeue();
-                if(item.GetType() == typeof(Braket))
-                {
-                    Braket braket = (Braket)item;
-                    if(braket.Open)
-                    {
-                        left.Value = op.Compute(left, Evaluate());
-                    }
-                    else
-                    {
-                        return left;
-                    }
-                }
-                else
-                {
-                    Number right = (Number)item;
-                    left.Value = op.Compute(left, right);                
-                }
-            }
-            return left;
+            return this; // Simply returns self.
         }
     }
 
+    /// <summary>
+    /// Type: Operator (Abstract)
+    /// Base type for binary operators.
+    /// </summary>
+    internal abstract class Operator : EComponent
+    {
+        public EComponent Left { get; set; }
+        public EComponent Right { get; set; }
+        public abstract Number Evaluate();
+    }
+
+    /// <summary>
+    /// Type: Add
+    /// Operator addition
+    /// Evaluates the sum of left and right operands.
+    /// </summary>
+    internal class Add : Operator
+    {
+        public override Number Evaluate()
+        {
+            Number temp = new Number();
+            temp.Value = Left.Evaluate().Value + Right.Evaluate().Value;
+            return temp;
+        }
+    }
+
+    /// <summary>
+    /// Type: Multiply
+    /// Operator multiplicataion
+    /// Evaluates the product of left and right operands.
+    /// </summary>
+    internal class Multiply : Operator
+    {
+        public override Number Evaluate()
+        {
+            Number temp = new Number();
+            temp.Value = Left.Evaluate().Value * Right.Evaluate().Value;
+            return temp;
+        }
+    }
+
+    /// <summary>
+    /// Type: Expression
+    /// Complex data structure that can hold multiple expression components.
+    /// Any sub-expression, usually in between (parenthesis) are also represented
+    /// by this data structure.
+    /// </summary>
+    internal class Expression : EComponent
+    {
+        /// <summary>
+        /// Components queue.
+        /// </summary>
+        protected Queue<EComponent> components = new Queue<EComponent>();
+
+        public Queue<EComponent> Components { get { return components; } }
+
+        /// <summary>
+        /// Strategy Pattern (composition)
+        /// Delegates how an expression is evaluated such as the precedence of operators.
+        /// </summary>
+        protected ExpressionEvaluateMethod method = null;
+
+        /// <summary>
+        /// Generic Regex to tokenize mathematical equation that can separate numeric tokens
+        /// (including real values) and some basic operators (*, +, /, -) and parenthesis.
+        /// </summary>
+        private readonly static string ExpressionRegex = @"([*+/\-)(])|([0-9.]+)";
+
+        /// <summary>
+        /// Constructor for the primary expression.
+        /// </summary>
+        /// <param name="method"></param>
+        public Expression(ExpressionEvaluateMethod method = null) 
+        {
+            if (method == null) this.method = new Part2Evaluate();
+            else this.method = method;
+        }
+
+        /// <summary>
+        /// Constructor for the seconday expressions (sub-expressions)
+        /// This constructor receives the components queue from the primary expression.
+        /// </summary>
+        /// <param name="expressionParts"></param>
+        /// <param name="method"></param>
+        public Expression(Queue<string> expressionParts, ExpressionEvaluateMethod method = null)
+        {
+            if (method == null) this.method = new Part2Evaluate();
+            else this.method = method;
+            Parse(expressionParts);
+        }
+
+        /// <summary>
+        /// Method: Parse
+        /// Tokenize string representation of mathematical expression into a string queue
+        /// </summary>
+        /// <param name="expression"></param>
+        public void Parse(string expression)
+        {
+            Queue<string> parts = new Queue<string>();
+            foreach (var temp in Regex.Matches(expression, ExpressionRegex))
+            {
+                parts.Enqueue(temp.ToString());
+            }
+            Parse(parts);
+        }
+
+        /// <summary>
+        /// Method: Parse
+        /// Parses string components into EComponent data structures.
+        /// </summary>
+        /// <param name="expressionParts"></param>
+        public void Parse(Queue<string> expressionParts)
+        {
+            while(expressionParts.Count > 0)
+            {
+                var item = expressionParts.Dequeue();
+                switch (item)
+                {
+                    case "+": components.Enqueue(new Add());
+                        break;
+                    case "*": components.Enqueue(new Multiply());
+                        break;
+                    case "(": components.Enqueue(new Expression(expressionParts, method));
+                        break;
+                    case ")": return;
+                    default: components.Enqueue(new Number(item));
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method: Evaluate
+        /// Delegation.
+        /// </summary>
+        /// <returns></returns>
+        public Number Evaluate()
+        {
+            return method.Evaluate(this);
+        }
+    }
+
+    /// <summary>
+    /// Interface: ExpressionEvaluateMethod
+    /// </summary>
+    internal interface ExpressionEvaluateMethod
+    {
+        public Number Evaluate(Expression expression);
+    }
+
+    /// <summary>
+    /// Type: Part1Evaluate
+    /// </summary>
+    internal class Part1Evaluate : ExpressionEvaluateMethod
+    {
+        /// <summary>
+        /// Method: Evaluate
+        /// No operator precedences
+        /// Evaluate left to right.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public Number Evaluate(Expression expression)
+        {
+            var queue = expression.Components;
+
+            EComponent left = null;
+            EComponent item = null;
+
+            while (queue.Count > 0)
+            {
+                item = queue.Dequeue();
+                if (item.GetType() == typeof(Number) || item.GetType() == typeof(Expression))
+                {
+                    left = item;
+                }
+                else
+                {
+                    Operator op = (Operator)item;
+                    op.Left = left;
+                    op.Right = queue.Dequeue();
+                    left = op;
+                }
+            }
+            return left.Evaluate();
+        }
+    }
+
+    /// <summary>
+    /// Type: Part2Evaluate
+    /// </summary>
+    internal class Part2Evaluate : ExpressionEvaluateMethod
+    {
+        /// <summary>
+        /// Method: Evalute
+        /// + first then *
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public Number Evaluate(Expression expression)
+        {
+            var queue = expression.Components;
+
+            EComponent item = null;
+            EComponent left = null;
+            EComponent root = null;
+
+            while (queue.Count > 0)
+            {
+                item = queue.Dequeue();
+                if (item.GetType() == typeof(Number) || item.GetType() == typeof(Expression))
+                {
+                    left = item;
+                }
+                else
+                {
+                    if (item.GetType() == typeof(Add))
+                    {
+                        Operator op = new Add();
+                        op.Left = left;
+                        op.Right = queue.Dequeue();
+                        left = op;
+                        root = op;
+                    }
+                    else if (item.GetType() == typeof(Multiply))
+                    {
+                        Operator op = new Multiply();
+                        op.Left = (root == null) ? left : root;
+                        op.Right = BuildRightTree();
+                        root = op;
+                        left = op;
+                    }
+                }
+            }
+            return root.Evaluate();
+
+            EComponent BuildRightTree()
+            {
+                EComponent temp = null;
+                EComponent number = null;
+                while (queue.Count > 0 && queue.Peek().GetType() != typeof(Multiply))
+                {
+                    temp = queue.Dequeue();
+                    if (temp.GetType() != typeof(Add))
+                    {
+                        number = temp;
+                    }
+                    else
+                    {
+                        if (temp.GetType() == typeof(Add))
+                        {
+                            var op = (Operator)temp;
+                            op.Left = number;
+                            op.Right = queue.Dequeue();
+                            number = op;
+                        }
+                    }
+                }
+                return number;
+            }
+        }
+    }
+    /// <summary>
+    /// Type: Day18Part1Solution
+    /// Part1 Solution
+    /// </summary>
     internal class Day18Part1Solution
     {
         public long Compute(string fileName)
@@ -130,13 +312,37 @@ namespace AdventOfCode2020.Day18
             long result = 0;
             foreach (var line in lines)
             {
-                var solution = new Expression();
+                var solution = new Expression(new Part1Evaluate());
                 solution.Parse(line);
                 var temp = solution.Evaluate().Value;
                 log.Append($"{line} : {temp}\n");
                 result += temp;
             }
             log.LogOnFile("Day18_Part1.txt");
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Type: Day18Part2Solution
+    /// Part2 Solution
+    /// </summary>
+    internal class Day18Part2Solution
+    {
+        public long Compute(string fileName)
+        {
+            LogStat log = new LogStat();
+            var lines = File.ReadAllLines(fileName);
+            long result = 0;
+            foreach (var line in lines)
+            {
+                var solution = new Expression(new Part2Evaluate());
+                solution.Parse(line);
+                var temp = solution.Evaluate().Value;
+                log.Append($"{line} : {temp}\n");
+                result += temp;
+            }
+            log.LogOnFile("Day18_Part2.txt");
             return result;
         }
     }
--- AdventOfCode2020/Day18/AdventDay18Test.cs
+++ AdventOfCode2020/Day18/AdventDay18Test.cs
@@ -10,13 +10,16 @@ namespace AdventOfCode2020.Day18
 {
     internal class AdventDay18Test
     {
+        /// <summary>
+        /// Part 1 Unit Testings.
+        /// </summary>
         [Test]
         public void FirstUnitTest()
         {
             string expr = "2 * 3 + (4 * 5)";
             string expr2 = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
 
-            Expression test = new Expression();
+            Expression test = new Expression(new Part1Evaluate());
             test.Parse(expr);
             Assert.AreEqual(26, test.Evaluate().Value);
             test.Parse(expr2);
@@ -28,6 +31,51 @@ namespace AdventOfCode2020.Day18
         {
             Day18Part1Solution solution = new Day18Part1Solution();
             Console.WriteLine($"Answer is {solution.Compute("AdventDay18Data.txt")}");
+                                // Answer is 2743012121210
+
+        }
+
+        /// <summary>
+        /// Part 2 Unit Testings.
+        /// </summary>
+        [Test]
+        public void SecondUnitTest()
+        {
+            string expr = "1 + 2 * 3 + 4 * 5 + 6";
+            string expr2 = "1 + (2 * 3) + (4 * (5 + 6))";
+            string expr3 = "2 * 3 + (4 * 5)";
+            string expr4 = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+            string expr5 = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+            string expr6 = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+
+            Expression test = new Expression(new Part2Evaluate());
+
+            test.Parse(expr);
+            Assert.AreEqual(231, test.Evaluate().Value);
+
+            test.Parse(expr2);
+            Assert.AreEqual(51, test.Evaluate().Value);
+
+            test.Parse(expr3);
+            Assert.AreEqual(46, test.Evaluate().Value);
+
+            test.Parse(expr4);
+            Assert.AreEqual(1445, test.Evaluate().Value);
+
+            test.Parse(expr5);
+            Assert.AreEqual(669060, test.Evaluate().Value);
+
+            test.Parse(expr6);
+            Assert.AreEqual(23340, test.Evaluate().Value);
+        }
+
+        [Test]
+        public void Part2Solution()
+        {
+            Day18Part2Solution solution = new Day18Part2Solution();
+            Console.WriteLine($"Answer is {solution.Compute("AdventDay18Data.txt")}");
+                                // Answer is 65658760783597
+
         }
     }
 }
