2b62bbdeafcb25fc383d1e99b4989ffee93e1ef5
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-02-14 16:55:08 -0800

    Comments are in.

--- AdventOfCode2020/Day22/AOC_Day22.cs
+++ AdventOfCode2020/Day22/AOC_Day22.cs
@@ -56,17 +56,24 @@ Player 2:
         }
     }
 
+    /// <summary>
+    /// Strategy Pattern (Composition)
+    /// Part1 and Part2 game rules changes and extends this class.
+    /// </summary>
     abstract class GameRule
     {
+        // Constans.
         internal readonly int PlayerOne = 0;
         internal readonly int PlayerTwo = 1;
 
+        // Player 1 and Player 2 Card Decks
         protected Deck[] decks = new Deck[2];
 
         protected bool IsGameOver()
         {
             return (decks[PlayerOne].IsEmpty() || decks[PlayerTwo].IsEmpty());
         }
+
         protected void CloneDecks(IEnumerable<Deck> players, int nP1Cards, int nP2Cards)
         {
             decks[PlayerOne] = new Deck("Player 1");
@@ -74,15 +81,29 @@ Player 2:
             this.decks[PlayerOne].LoadDeck(players.First().ExtractCardValues(nP1Cards));
             this.decks[PlayerTwo].LoadDeck(players.Last().ExtractCardValues(nP2Cards));
         }
+
+        /// <summary>
+        /// Commence Crab Card Game.
+        /// </summary>
+        /// <param name="players">decks of cards for player 1 and 2</param>
+        /// <param name="nPlayer1Deck">how many cards to use from the deck for player one</param>
+        /// <param name="nPlayer2Deck">number of cards to use from the deck for player two</param>
+        /// <returns></returns>
         public abstract Deck Play(IEnumerable<Deck> players, int nPlayer1Deck = 0, int nPlayer2Deck = 0);
     }
 
+    /// <summary>
+    /// Part One Game Rule: Higher Face Value card wins.
+    /// </summary>
     internal class Part1Rule : GameRule
     {
         private void PlayRound()
         {
+            // Take a card from each deck.
             var player1Hand = decks[PlayerOne].DequeueCard();
             var player2Hand = decks[PlayerTwo].DequeueCard();
+
+            // There are no cards with same value. Thus no reason to consider draw case.
             if (player1Hand.FaceValue > player2Hand.FaceValue)
             {
                 decks[PlayerOne].EnqueueCard(player1Hand);
@@ -96,16 +117,21 @@ Player 2:
         }
         public override Deck Play(IEnumerable<Deck> players, int nPlayer1Deck = 0, int nPlayer2Deck = 0)
         {
+            // New Decks For this Game.
             CloneDecks(players, nPlayer1Deck, nPlayer2Deck);
             while (!IsGameOver())
             {
                 PlayRound();
             }
+            // Return winner's Deck
             return (decks[PlayerOne].IsEmpty()) ? decks[PlayerTwo] : decks[PlayerOne];
 
         }
     }
 
+    /// <summary>
+    /// Player Deck as integer values for pattern mathching.
+    /// </summary>
     internal class DeckData
     {
         private readonly int[] faceValues;
@@ -127,10 +153,22 @@ Player 2:
         }
     }
 
+    /// <summary>
+    /// Recored previous hands for a player.
+    /// Uses hashing with first card face value in an attempt to reduce comaprison.
+    /// </summary>
     internal class DeckRecorder
     {
         private Dictionary<int, List<DeckData>> records = new Dictionary<int, List<DeckData>>();
 
+        /// <summary>
+        /// Record a deck's face values at the moment.
+        /// If no such sequence is found, simply record and return true, but if the same sequence was recoreded previous, 
+        /// return false.
+        /// </summary>
+        /// <param name="firstCardValue"></param>
+        /// <param name="restValues"></param>
+        /// <returns></returns>
         public bool Record(int firstCardValue, int[] restValues)
         {
             if(records.ContainsKey(firstCardValue))
@@ -154,14 +192,23 @@ Player 2:
         }
     }
 
+    /// <summary>
+    /// Part 2 Game Rule.
+    /// Requires recording previous hands, and initializing sub games.
+    /// </summary>
     internal class Part2Rule : GameRule
     {
+        // Record Keepers
         DeckRecorder player1History = new DeckRecorder();
         DeckRecorder player2History = new DeckRecorder();
 
-
-        private StringBuilder history = new StringBuilder();
-
+        /// <summary>
+        /// Determines whether player 1 won in this round.
+        /// Create a sub game when necessary.
+        /// </summary>
+        /// <param name="player1Hand"></param>
+        /// <param name="player2Hand"></param>
+        /// <returns></returns>
         private bool DidPlayerOneWin(SpaceCard player1Hand, SpaceCard player2Hand)
         {
             // Rule 2: winner of sub-game
@@ -169,7 +216,9 @@ Player 2:
                 decks[PlayerTwo].Count >= player2Hand.FaceValue)
             {
                 var subGame = new Part2Rule();
+                // Start the sub game with limited cards according to their current hands' value.
                 var result = subGame.Play(decks, player1Hand.FaceValue, player2Hand.FaceValue);
+
                 if (result.Name.Contains("1")) return true;
                 else return false;
             }
@@ -179,25 +228,28 @@ Player 2:
                 return player1Hand.FaceValue >= player2Hand.FaceValue;
             }
         }
+
+        /// <summary>
+        /// Play One Round of Match.
+        /// </summary>
         private void PlayRound()
         {
+            // Take a card from each deck.
             var player1Hand = decks[PlayerOne].DequeueCard();
             var player2Hand = decks[PlayerTwo].DequeueCard();
 
-            // Rule 1: repeat sequence
-
+            // Rule 1: check repeat deck sequence
             var p1Record = player1History.Record(player1Hand.FaceValue, decks[PlayerOne].ExtractCardValues(0));
             var p2Record = player2History.Record(player2Hand.FaceValue, decks[PlayerTwo].ExtractCardValues(0));
 
-
+            // If Rule 1 is violated, then end this game as player one's win.
             if(!(p1Record && p2Record))
             {
-                decks[PlayerOne].EnqueueCard(player1Hand);
-                decks[PlayerOne].EnqueueCard(player2Hand);
                 decks[PlayerTwo].Clear();
                 return;
             }
 
+            // Place the current cards to the winner's deck.
             if (DidPlayerOneWin(player1Hand, player2Hand))
             {
                 decks[PlayerOne].EnqueueCard(player1Hand);
@@ -221,14 +273,23 @@ Player 2:
         }
     }
 
-
+    /// <summary>
+    /// Crab Combat Game using Space Cards.
+    /// </summary>
     internal class Combat
     {
+        // Constants.
         internal readonly int PlayerOne = 0;
         internal readonly int PlayerTwo = 1;
 
+        // Decks for players 1 and 2
         private List<Deck> decks = new List<Deck>();
 
+        /// <summary>
+        /// Parser
+        /// </summary>
+        /// <param name="cardData"></param>
+        /// <exception cref="Exception"></exception>
         public void DealCards(string[] cardData)
         {
             Deck currentDeck = null;
@@ -250,6 +311,11 @@ Player 2:
             }
         }
 
+        /// <summary>
+        /// Let the game begin.
+        /// </summary>
+        /// <param name="playRule"></param>
+        /// <returns></returns>
         public Deck Play(GameRule playRule)
         {
             return playRule.Play(decks.ToArray());
@@ -280,6 +346,9 @@ Player 2:
         }
     }
 
+    /// <summary>
+    /// Space Card
+    /// </summary>
     internal class SpaceCard
     {
         private readonly int value;
@@ -292,8 +361,12 @@ Player 2:
         }
     }
 
+    /// <summary>
+    /// Deck of Space Cards
+    /// </summary>
     internal class Deck
     {
+        // Player Name here
         public string Name { get; init; }
 
         public int Count { get { return cards.Count; } }
@@ -329,6 +402,12 @@ Player 2:
             return cards.Peek();
         }
 
+        /// <summary>
+        /// Consider this method as a Peek() function that can return multiple items.
+        /// Mainly used for recording player deck and cloning deck for subgames.
+        /// </summary>
+        /// <param name="nElements"></param>
+        /// <returns></returns>
         public int[] ExtractCardValues(int nElements = 0)
         {
             var temp = new List<int>();
