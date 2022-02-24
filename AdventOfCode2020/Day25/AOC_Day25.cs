4fee0908c351813001d1d0cc752f06b8d117d4ee
yongjohnlee80 <yongjohnlee80@outlook.com>
2022-02-23 22:27:38 -0800

    AOC 2020 Done and Done!

--- AdventOfCode2020/Day25/AOC_Day25.cs
+++ AdventOfCode2020/Day25/AOC_Day25.cs
@@ -13,16 +13,22 @@ namespace AdventOfCode2020.Day25
         [Test]
         public void TestPart1Solution()
         {
-            var keyCard = new RFIDevice(2084668);
-            var doorLock = new RFIDevice(3704642);
+            var keyCard = new RFIDevice(PublicKey: 2084668);
+            var doorLock = new RFIDevice(PublicKey: 3704642);
 
-            ComboBreaker lockPicker = new ComboBreaker();
+            var AOC_ResortRFIDScanner = new ComboBreaker();
 
-            var scannedKeyCard = lockPicker.BreakEncryptionKey(keyCard, doorLock);
-            var scannedDoorLock = lockPicker.BreakEncryptionKey(doorLock, keyCard);
+            var scannedKeyCard = AOC_ResortRFIDScanner.BreakEncryptionKey(keyCard, doorLock);
 
-            Assert.That(scannedKeyCard.EncryptionKey, Is.EqualTo(scannedDoorLock.EncryptionKey));
-            Console.WriteLine(scannedKeyCard.EncryptionKey);
+            // Don't really need to break the door lock, but hey, why not?
+            var scannedDoorLock = AOC_ResortRFIDScanner.BreakEncryptionKey(doorLock, keyCard);
+
+            Console.WriteLine($"{scannedKeyCard.LoopSize}:{scannedDoorLock.LoopSize}");
+
+            Assert.That(scannedKeyCard.EncryptionKey, Is.EqualTo(scannedDoorLock.EncryptionKey)); 
+                            // Both should be equal to establish handshake.
+
+            Assert.That(scannedKeyCard.EncryptionKey, Is.EqualTo(42668));
         }
 
         [Test]
@@ -54,7 +60,7 @@ namespace AdventOfCode2020.Day25
     /// </summary>
     internal class ComboBreaker
     {
-        private readonly int Constant = 20201227;
+        private readonly int Constant = 20201227; // Some provided constant key value.
 
         /// <summary>
         /// Cracks the value of internal loop size of the RFIDevice by bruteforce.
@@ -90,9 +96,9 @@ namespace AdventOfCode2020.Day25
             int loopSize;
 
             // If the internal loop size cannot be detected, then crack it.
-            // Of course, we can raise an exception on such occasion, but why?
-            // What we are doing it illegal anyways, so, let's just get it done quick and
-            // rest on the bed.
+            // Of course, we can raise an exception on such occasions, but why?
+            // What we are doing is illegal anyways; so, let's just get it done quickly and
+            // get some rest inside.
             if(device.LoopSize == 0)
             {
                 var temp = BreakLoopSize(device);
